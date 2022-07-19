9260972de4ed4115e2dec25a6a7bf64b1613ba01
vachhaninimit <vachhaninimit@gmail.com>
2022-07-19 19:19:15 +0530

    Add files via upload

---
+++ MDK-ARM/RTE/_STM32-L-151-RE_GB_TRI_AXIS_RS-485_RTU_TCPIP/RTE_Components.h
@@ -0,0 +1,26 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'STM32-L-151-RE GB TRI AXIS RS-485 RTU TCPIP' 
+ * Target:  'STM32-L-151-RE GB TRI AXIS RS-485 RTU TCPIP' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32l1xx.h"
+
+/* lwIP.lwIP::Network:CORE:IPv4:2.1.2 */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_IPv4                /* Network IPv4 Stack */
+/* lwIP.lwIP::Network:Interface:Ethernet:2.1.2 */
+#define RTE_Network_Interface_Ethernet  /* Network Interface Ethernet */
+
+
+#endif /* RTE_COMPONENTS_H */
